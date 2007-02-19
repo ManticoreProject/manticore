0cfc9bdbbf60b27fd4c52ab03b71176d7ab5b929
mrainey <mrainey@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2007-02-19 00:55:05 +0000

    Now saves callee-save regs.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@307 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/runtime/mantentry.s
+++ src/lib/runtime/mantentry.s
@@ -14,7 +14,23 @@ mantentryglue:
 	movq %rdi, %rax
 	xorl %edi, %edi
 	xorl %ebx, %ebx
+	/* save the callee-save regs*/
+	pushq %rbx
+	pushq %rbp
+	pushq %r12
+	pushq %r13
+	pushq %r14
+	pushq %r15
+	/* */
 	jmp mantentry    /* entry function for CFG programs */
 returnloc:
+	/* restore the callee-save regs*/
+	popq %r15
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %rbp	
+	popq %rbx
+	/* */	
 	ret
 	
