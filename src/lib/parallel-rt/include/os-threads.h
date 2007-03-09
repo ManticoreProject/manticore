c9ce8cbd2e0065efd2acd2b9c16a211a0c7fd47b
mrainey <mrainey@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2007-03-09 21:45:43 +0000

    Fixed argument-passing bug in ThreadCreate.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@349 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/include/os-threads.h
+++ src/lib/parallel-rt/include/os-threads.h
@@ -120,7 +120,7 @@ typedef pthread_t OSThread_t;
 
 STATIC_INLINE bool ThreadCreate (OSThread_t *tidOut, void * (*f)(void *), void *arg)
 {
-    if (pthread_create(tidOut, (pthread_attr_t *)0, f, &arg) != 0)
+    if (pthread_create(tidOut, (pthread_attr_t *)0, f, arg) != 0)
 	return false;
     else
 	return true;
