8112b45647d2cc0eacdf99b652cccf40224d3789
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2007-03-24 03:21:09 +0000

    Add a place to put C functions that we might want to call.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@473 ac4fbcad-d21d-0410-b760-c7b1d3380187

---
+++ src/lib/parallel-rt/misc/basis.c
@@ -0,0 +1,15 @@
+/* basis.c
+ *
+ * COPYRIGHT (c) 2007 The Manticore Project (http://manticore.cs.uchicago.edu)
+ * All rights reserved.
+ */
+
+#include "manticore-rt.h"
+
+/* M_Print:
+ */
+void M_Print (char *s)
+{
+    Say(s);
+}
+
