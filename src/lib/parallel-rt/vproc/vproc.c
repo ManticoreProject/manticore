bbb163533984a664c1ca60b58a73ea53bb5a130a
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2007-11-15 20:18:17 +0000

    Recommitting fix for Leopard, which somehow was undone in verson 1439.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@1492 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/vproc/vproc.c
+++ src/lib/parallel-rt/vproc/vproc.c
@@ -52,8 +52,8 @@ extern int ASM_VProcSleep;
 #  define UC_RIP(uc)	((uc)->uc_mcontext.gregs[REG_RIP])
 #elif defined(TARGET_DARWIN)
 #  if defined(__DARWIN_UNIX03)
-#    define UC_R11(uc)	((uc)->uc_mcontext->ss.r11)
-#    define UC_RIP(uc)	((uc)->uc_mcontext->ss.rip)
+#    define UC_R11(uc)	((uc)->uc_mcontext->__ss.__r11)
+#    define UC_RIP(uc)	((uc)->uc_mcontext->__ss.__rip)
 #  else
 #    define UC_R11(uc)	((uc)->uc_mcontext->ss.r11)
 #    define UC_RIP(uc)	((uc)->uc_mcontext->ss.rip)
