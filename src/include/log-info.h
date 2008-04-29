24853f6c334537ddba39dacda5cd8c254b6ead5e
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-04-29 18:42:08 +0000

    Starting to add some type information for log messages
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@2072 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/include/log-info.h
+++ src/include/log-info.h
@@ -21,4 +21,28 @@ typedef enum {
 			/* start of the interval */
 } LogEventKind_t;
 
+/* Different formats of log messages.  A log message can have upto 20 bytes of
+ * data; values can be 32-bit integers or floats and 64-bit integers or floats.
+ * We also allow 32-bit unused values.  The 64-bit data values must be aligned
+ * on 8-byte boundaries.  We represent the "type" signature of a log message as
+ * a 6-character string (5 32-bit fields plus the '\0' terminator).  The field
+ * values have the following interpretation:
+ *
+ *	u	-- unused (padding)
+ *	i	-- 32-bit integer
+ *	f	-- 32-bit float
+ *	I	-- half of a 64-byte integer
+ *	F	-- half of a 64-byte float
+ */
+
+typedef char LogMsgType_t[6];
+
+typedef enum {
+    LOG_DATA_UNUSED = 'u',
+    LOG_DATA_INT32 = 'i',
+    LOG_DATA_INT64 = 'I',
+    LOG_DATA_FLOAT32 = 'f',
+    LOG_DATA_FLOAT64 = 'F'
+};
+
 #endif /* !_LOG_INFO_H_ */
