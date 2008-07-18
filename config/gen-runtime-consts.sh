d370e6608c23e49a0f07a262d94ea2833c9f84e2
mrainey <mrainey@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-07-18 21:34:37 +0000

    Generate offsets for library code.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@2201 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- config/gen-runtime-consts.sh
+++ config/gen-runtime-consts.sh
@@ -23,5 +23,8 @@ function gen {
 gen gen-runtime-constants src/tools/mc/driver/runtime-constants.sml
 gen gen-runtime-offsets src/lib/hlops/include/runtime-offsets.def
 gen gen-log-events src/lib/hlops/include/log-events.def
+# moving away from hlop files
+gen gen-runtime-offsets src/lib/basis/runtime/runtime-offsets.def
+gen gen-log-events src/lib/basis/runtime/log-events.def
 
 exit 0
