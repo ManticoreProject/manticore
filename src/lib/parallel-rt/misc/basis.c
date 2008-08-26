74ddbbb9a2e090b1f7e7b6d472fc98b1f971556e
mrainey <mrainey@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-08-26 21:16:01 +0000

    Bug
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@2303 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/misc/basis.c
+++ src/lib/parallel-rt/misc/basis.c
@@ -289,9 +289,9 @@ Word_t M_Random (Word_t lo, Word_t hi)
     return (random() % (hi - lo)) + lo;
 }
 
-int32_t M_Random (int32_t lo, int32_t hi)
+int32_t M_RandomInt (int32_t lo, int32_t hi)
 {
-  return (int)M_Random((int)lo, (int)hi);
+  return (int)M_RandomInt((int)lo, (int)hi);
 }
 
 void M_SeedRand ()
