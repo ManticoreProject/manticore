a99febae3ba9024913a17451e39c01081f559fb0
mrainey <mrainey@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-09-15 04:14:17 +0000

    Indentation.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@2354 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/vproc/apply.c
+++ src/lib/parallel-rt/vproc/apply.c
@@ -84,9 +84,9 @@ void RunManticore (VProc_t *vp, Addr_t codeP, Value_t arg, Value_t envP)
 	  /* check for pending signals */
 	    if ((vp->sigPending == M_TRUE) && (vp->atomic == M_FALSE)) {
 		Value_t resumeK = AllocUniform(vp, 3,
-			    PtrToValue(&ASM_Resume),
-			    vp->stdCont,
-			    vp->stdEnvPtr);
+					       PtrToValue(&ASM_Resume),
+					       vp->stdCont,
+					       vp->stdEnvPtr);
 
 		/* pass the signal to scheduling code in the BOM runtime; for more details, see
 		 * the comments in src/lib/basis/runtime/scheduler-utils.pml. */
