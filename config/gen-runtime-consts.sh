affd9fdf4e293057098293327939f7ca01703e43
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-10-06 17:13:40 +0000

    Put generated files in include directory
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@2391 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- config/gen-runtime-consts.sh
+++ config/gen-runtime-consts.sh
@@ -24,7 +24,7 @@ gen gen-runtime-constants src/tools/mc/driver/runtime-constants.sml
 gen gen-runtime-offsets src/lib/hlops/include/runtime-offsets.def
 gen gen-log-events src/lib/hlops/include/log-events.def
 # moving away from hlop files
-gen gen-runtime-offsets src/lib/basis/runtime/runtime-offsets.def
-gen gen-log-events src/lib/basis/runtime/log-events.def
+gen gen-runtime-offsets src/lib/basis/include/runtime-offsets.def
+gen gen-log-events src/lib/basis/include/log-events.def
 
 exit 0
