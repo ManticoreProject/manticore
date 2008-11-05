3f9a93b3b33212fd5b317fe6a6d0c29d76e51bf8
mrainey <mrainey@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-11-05 20:09:37 +0000

    C runtime support for work stealing local deques.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/branches/sw-polling@2576 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/misc/work-stealing-local-deques.c
+++ src/lib/parallel-rt/misc/work-stealing-local-deques.c
@@ -19,44 +19,122 @@
 
 #define DEQUE_LEN 1024
 
-/* local work-stealing deque */
+/* local, work-stealing deque */
 typedef struct {
-  int hd;                      /* pointer to the head of the deque */
-  int tl;                      /* pointer to the tail of the deque */
-  Value_t elts[DEQUE_LEN];     /* memory for the deque */
+  int hd;                             /* pointer to the head of the deque */
+  int tl;                             /* pointer to the tail of the deque */
+  Value_t elts[DEQUE_LEN];            /* memory for the deque */
 } WSLocalDeque_t;
 
-/*! \brief add the deque elements to the root set.
+/* list of worker deques */
+struct WSLocalDeques_s {
+  WSLocalDeque_t* hd;                 /* local deque */
+  struct WSLocalDeques_s* tl;         /* rest of the list */
+  Bool_t live;                        /* if this field is false, then the local deque is garbage */
+};
+
+typedef struct WSLocalDeque_s WSLocalDeques_t;
+
+/* one list of local deques per vproc */
+WSLocalDeques_t* globalLists[] = NULL;
+
+/* \brief prune the dead local deques
+ * \param localDeques list of local deques
+ * \param pruned list of already pruned local deques
+ * \return pruned list of local deques
+ */
+static WSLocalDeques_t* PruneLocalDequesLoop (WSLocalDeques_t* localDeques, WSLocalDeques_t* pruned)
+{
+  if (localDeques == M_NIL) {
+    return pruned;
+  } else if (localDeques->live == M_TRUE) {
+    WSLocalDeques_t* tl = localDeques->tl;
+
+    localDeques->tl = pruned;
+
+    return PruneLocalDequesLoop(tl, localDeques);
+  } else {
+    WSLocalDeques_t* tl = localDeques->tl;
+    
+    FREE(localDeques);
+
+    return PruneLocalDeques(tl, pruned);
+  }
+}
+
+/* \brief prune dead local deques from the global list
+ * \param vprocId unique id of the host vproc
+ * \return pruned pruned list of local deques
+ */
+static WSLocalDeques_t* PruneLocalDeques (int vprocId)
+{
+  /* TODO */  
+}
+
+/*! \brief add the deque elements to the root set; at this point we prune out deques that are no longer live.
  *  \param vp the host vproc
  *  \param rp the root-set pointer
  */
-Value_t** M_WSAddLocalDequeRoots (VProc_t *vp, Value_t **rp)
+Value_t** M_WSAddLocalDequeRoots (VProc_t* vp, Value_t** rp)
 {
-  /* TODO: find a way to access the active local deques */
-  /*
-  WSLocalDeque_t* deque = &(deques[vp->id]);
-  for (int i = deque->hd; i < deque->tl; i++) {
-    *rp++ = &(deque->elts[i]);
+  WSLocalDeques_t* globalList = PruneLocalDeques(vp->id);
+
+  while (globalList != M_NIL) {
+    WSLocalDeque_t* localDeque = globalList->hd;
+
+    for(int i = localDeque->hd; i < localDeque->tl; i++) {
+      *rp++ = &(localDeque->elts[i]);
+    }
+
+    globalList = globalList->tl;
+
   }
-  */
+
   return rp;
 }
 
-/* \brief initialize the local deques
-   \param vp the host vproc
-   \param n the number of local deques to initialize
+/* \brief add the local deque to the globally-maintained list of local deques
+ * \param vprocId unique id of the host vproc
+ * \param localDeque the local deque
+ * \return entry in the global list
  */
-Value_t M_WSAllocLocalDeques (VProc_t *vp, int n)
+static WSLocalDeques* AddToGlobalList (int vprocId, WSLocalDeque_t* localDeque)
 {
-  WSLocalDeque_t* deques = (WSLocalDeque_t*)ValueToPtr(AllocUniform(vp, n * sizeof(WSLocalDeque_t)));
+  WSLocalDeques_t* globalList = NEW(WS_LocalDeques_t);
 
-  for (int i = 0; i < n; i++) {
-    deques[i].hd = 0;
-    deques[i].tl = 0;
-    for (int j = 0; j < DEQUE_LEN; j++) {
-      deques[i].elts[j] = M_NIL;
-    }
+  globalList->hd = localDeque;
+  globalList->tl = globalLists[vprocId];
+  globalList->live = M_TRUE;
+
+  globalLists[vprocId] = globalList;
+
+  return globalList;
+}
+
+/* \brief allocate and initialize a local deque
+ * \return new local deque
+ */
+static WSLocalDeque_t* AllocLocalDeque ()
+{
+  WSLocalDeque_t* deque = NEW(WSLocalDeque_t);
+
+  deque->tl = deque->hd = 0;
+
+  for (int i = 0; i < DEQUE_LEN; i++) {
+    deque->elts[i] = M_NIL;
   }
 
-  return (Value_t)deques;
+  return deque;
+}
+
+/* \brief allocate a local deque
+ * \param the host vproc id
+ * \return allocated and initialized local deque
+ */
+Value_t M_WSAllocLocalDeque (int vprocId)
+{
+  WSLocalDeque_t* localDeque = AllocLocalDeque();
+  WSLocalDeques_t* localDeques = AddToGlobalList(vprocId, localDeque);
+
+  return PtrToValue(localDeques);
 }
