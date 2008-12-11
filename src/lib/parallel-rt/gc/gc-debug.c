35cf7388b651699cbc768d7c1fa588bef49845c1
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2008-12-11 22:57:25 +0000

    Fix coding style
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@2824 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/gc/gc-debug.c
+++ src/lib/parallel-rt/gc/gc-debug.c
@@ -13,64 +13,67 @@
 #include "gc-inline.h"
 
 /*! \brief check for obviously corrupt objects in a heap
+ *  \param vp host vproc
  *  \param beginHeap low address of the heap
  *  \param endHeap high address of the heap
  */
 void HeapConsistencyCheck (VProc_t *vp, Word_t *beginHeap, Word_t *endHeap)
 {
-  Word_t        *nextScan = beginHeap;
-  Addr_t	nurseryBase = vp->nurseryBase;
-  Addr_t	allocSzB = vp->allocPtr - nurseryBase - WORD_SZB;
+    Word_t	*nextScan = beginHeap;
+    Addr_t	nurseryBase = vp->nurseryBase;
+    Addr_t	allocSzB = vp->allocPtr - nurseryBase - WORD_SZB;
 
-  while (nextScan < endHeap-1) {
-    Word_t hdr = *nextScan;	// get object header
-    if (isMixedHdr(hdr)) {
-      // a record
-      Word_t tagBits = GetMixedBits(hdr);
-      assert ((uint64_t)tagBits < (1l << (uint64_t)GetMixedSizeW(hdr)));
-      Value_t *scanP = (Value_t *)nextScan;
-      while (tagBits != 0) {
-	if (tagBits & 0x1) {
-	  Value_t v = *scanP;
-	  if (isPtr(v)) {
-	    if (inAddrRange(nurseryBase, allocSzB, ValueToAddr(v))) { 	      // pointer into the nursery
-	      // only young to old pointers allowed in the nursery
-	      assert(ValueToAddr(v) < (Addr_t)nextScan);
+    while (nextScan < endHeap-1) {
+	Word_t hdr = *nextScan;	// get object header
+	if (isMixedHdr(hdr)) {
+	  // a record
+	    Word_t tagBits = GetMixedBits(hdr);
+	    assert ((uint64_t)tagBits < (1l << (uint64_t)GetMixedSizeW(hdr)));
+	    Value_t *scanP = (Value_t *)nextScan;
+	    while (tagBits != 0) {
+		if (tagBits & 0x1) {
+		    Value_t v = *scanP;
+		    if (isPtr(v)) {
+			if (inAddrRange(nurseryBase, allocSzB, ValueToAddr(v))) { // pointer into the nursery
+			  // only young to old pointers allowed in the nursery
+			    assert(ValueToAddr(v) < (Addr_t)nextScan);
+			}
+			else {
+			  // raw value or object in an older generation
+			}
+		    }
+		}
+		tagBits >>= 1;
+		scanP++;
 	    }
-	    else {
-	      // raw value or object in an older generation
-	      }
-	  }
+	    nextScan += GetMixedSizeW(hdr);
 	}
-	tagBits >>= 1;
-	scanP++;
-      }
-      nextScan += GetMixedSizeW(hdr);
-    } else if (isVectorHdr(hdr)) {
-      // an array of pointers
-      int len = GetVectorLen(hdr);
-      for (int i = 0;  i < len;  i++, nextScan++) {
-	Value_t v = *(Value_t *)nextScan;
-	if (isPtr(v)) {
-	  if (inAddrRange(nurseryBase, allocSzB, ValueToAddr(v))) {           // pointer into the nursery
-	    // only young to old pointers allowed in the nursery
-	    assert(ValueToAddr(v) < (Addr_t)nextScan);
-	  }
-	  else {
-	    // object in an older generation
-	  }
+	else if (isVectorHdr(hdr)) {
+	  // an array of pointers
+	    int len = GetVectorLen(hdr);
+	    for (int i = 0;  i < len;  i++, nextScan++) {
+		Value_t v = *(Value_t *)nextScan;
+		if (isPtr(v)) {
+		    if (inAddrRange(nurseryBase, allocSzB, ValueToAddr(v))) {  // pointer into the nursery
+		      // only young to old pointers allowed in the nursery
+			assert(ValueToAddr(v) < (Addr_t)nextScan);
+		    }
+		    else {
+		      // object in an older generation
+		    }
+		}
+	    }
+	    nextScan += len;
 	}
-      }
-      nextScan += len;
-    } else if (isForwardPtr(hdr)) {
-      // object in an older generation
-      // TODO: check that the object in the older generation is consisten with this one
+	else if (isForwardPtr(hdr)) {
+	  // object in an older generation
+	  // TODO: check that the object in the older generation is consistent with this one
+	}
+	else {
+	  // we can just skip raw objects
+	    assert (isRawHdr(hdr));
+	    nextScan += GetRawSizeW(hdr);
+	}
     }
-    else {
-      // we can just skip raw objects
-      assert (isRawHdr(hdr));
-      nextScan += GetRawSizeW(hdr);
-    }
-  }
 
 }
