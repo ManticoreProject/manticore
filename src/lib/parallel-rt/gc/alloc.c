012f8d331bb6406be48d224e610c26dff7309757
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2009-05-19 16:36:19 +0000

    Code cleanup
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@3457 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/gc/alloc.c
+++ src/lib/parallel-rt/gc/alloc.c
@@ -199,7 +199,7 @@ Value_t GlobalAllocArray (VProc_t *vp, int nElems, Value_t elt)
 {
   /* the array must fit into a global chunk */
     if (HEAP_CHUNK_SZB <= WORD_SZB*(nElems+1))
-      Die ("GlobalAllocArray: Cannot allocate an array that is larger than the global chunk size.");
+	Die ("GlobalAllocArray: Cannot allocate an array that is larger than the global chunk size.");
 
     if (vp->globNextW + WORD_SZB * (nElems+1) >= vp->globLimit) {
 	AllocToSpaceChunk(vp);
@@ -215,11 +215,6 @@ Value_t GlobalAllocArray (VProc_t *vp, int nElems, Value_t elt)
     return PtrToValue(obj);
 }
 
-STATIC_INLINE int CeilingDivide (int x, int y)
-{
-  return x / y + (x % y > 0 ? 1 : 0);
-}
-
 /*! \brief allocate an array of floats in the global heap
  *  \param vp the host vproc
  *  \param nElems the number of elements in the array
@@ -228,10 +223,10 @@ STATIC_INLINE int CeilingDivide (int x, int y)
  */
 Value_t GlobalAllocFloatArray (VProc_t *vp, int nElems, float elt)
 {
-    int nWords = CeilingDivide (nElems * sizeof (float), WORD_SZB);
+    int nWords = BYTES_TO_WORDS(nElems * sizeof(float));
   /* the array must fit into a global chunk */
     if (HEAP_CHUNK_SZB <= WORD_SZB*(nElems+1))
-      Die ("GlobalAllocFloatArray: Cannot allocate an array that is larger than the global chunk size.");
+	Die ("GlobalAllocFloatArray: Cannot allocate an array that is larger than the global chunk size.");
 
     if (vp->globNextW + WORD_SZB * (nWords+1) >= vp->globLimit) {
 	AllocToSpaceChunk(vp);
@@ -254,9 +249,9 @@ Value_t GlobalAllocFloatArray (VProc_t *vp, int nElems, float elt)
  *  \param elt the initial value for the array elements
  *  \return pointer to the beginning of the array
  */
-Value_t GlobalAllocIntArray (VProc_t *vp, int nElems, int elt)
+Value_t GlobalAllocIntArray (VProc_t *vp, int nElems, int32_t elt)
 {
-    int nWords = CeilingDivide (nElems * sizeof (int), WORD_SZB);
+    int nWords = BYTES_TO_WORDS(nElems * sizeof(int32_t));
   /* the array must fit into a global chunk */
     assert(HEAP_CHUNK_SZB > WORD_SZB*(nWords+1));
 
