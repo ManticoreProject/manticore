b090e7a1e139e3d260a849d40cbae093ab68da2b
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2009-07-12 17:30:35 +0000

    Cleaned up the interface the the CRC32 computation and added offsets for the   log buffer and log events.
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/trunk@3896 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/lib/parallel-rt/config/crc.h
+++ src/lib/parallel-rt/config/crc.h
@@ -12,15 +12,25 @@
 #define _CRC_H_
 
 #include <string.h>
+#include <stdint.h>
 
 extern uint32_t CRC32 (void *buf, int nBytes);
 
-/* add an entry to the buffer that we'll compute the CRC of */
-static inline char *AddCRCData (char *buf, const char *lab, int offset)
+/*! \brief add an entry to the CRC buffer.
+ *  \param buf the pointer to the next location in the buffer
+ *  \param lab the symbol being added
+ *  \param offset the symbol's value
+ *  \return the next buffer location to store data.
+ */
+static inline char *AddCRCData (char *buf, const char *lab, uint64_t offset)
 {
     int n = strlen(lab);
     strncpy(buf, lab, n);
     buf += n;
+  // Note: we only record 32-bits of the offset, since the high bits will usually be 0
+  // (orelse all 1's).
+    *buf++ = ((offset >> 24) & 0xff);
+    *buf++ = ((offset >> 16) & 0xff);
     *buf++ = ((offset >> 8) & 0xff);
     *buf++ = offset & 0xff;
 
--- src/lib/parallel-rt/config/gen-runtime-offsets.c
+++ src/lib/parallel-rt/config/gen-runtime-offsets.c
@@ -1,42 +1,48 @@
-/* gen-runtime-constants.c
+/* gen-runtime-offsets.c
  *
- * COPYRIGHT (c) 2007 The Manticore Project (http://manticore.cs.uchicago.edu)
+ * COPYRIGHT (c) 2009 The Manticore Project (http://manticore.cs.uchicago.edu)
  * All rights reserved.
  *
  * Generate offsets into the runtime-system data structures that are used
- * in the scheduler HLOps.
+ * in the basis HLOps.
  */
 
 #include <stdlib.h>
 #include <stdbool.h>
 #include "manticore-rt.h"
 #include <stdio.h>
-#include <string.h>
 #include "vproc.h"
-#include "value.h"
-#include "../vproc/scheduler.h"
+#include "log-file.h"
+#include "crc.h"
 
-extern uint32_t CRC32 (void *buf, int nBytes);
+/* print the definition of a symbol */
+#define PR_DEFINE(symb, val)							\
+	printf("#define " #symb " %#0lx\n", (uint64_t)val)
 
-#define VP_OFFSET(obj, xxx, lab, local)						\
+/* print a value definition and record it in the CRC buffer */
+#define PR_VALUE(tag, var, value)						\
 	do {									\
-	    uint32_t _offset = (int)((Addr_t)&(obj.lab) - (Addr_t)&obj);	\
-	    strncpy((char *)(buf+len), #lab, sizeof(#lab));			\
-	    len += sizeof(#lab);						\
-	    buf[len++] = ((_offset >> 8) & 0xff);				\
-	    buf[len++] = _offset & 0xff;					\
-	    printf("#define " #xxx " %d\n", _offset);				\
+	    bp = AddCRCData (bp, #tag, value);					\
+	    PR_DEFINE(var, value);						\
 	} while (0)
 
-#define PR_DEFINE(symb, val)			\
-	printf("    val " #symb " : IntInf.int = %d\n", val)
+/* print a field offset and record it in the CRC buffer */
+#define PR_OFFSET(obj, var, lab)						\
+	do {									\
+	    uint32_t _offset = (int)((Addr_t)&(obj.lab) - (Addr_t)&obj);	\
+	    PR_VALUE(lab, var, _offset);					\
+	} while (0)
+
+/* print a VProc_t field offset and record it in the CRC buffer */
+#define VP_OFFSET(obj, xxx, lab, local)		PR_OFFSET(obj, lab, lab)
 
 int main ()
 {
     VProc_t		vp;
-    SchedActStkItem_t	actcons;
-    unsigned char		buf[1024];
-    int			len = 0;
+    LogBuffer_t		logBuffer;
+    LogEvent_t		logEvent;
+    unsigned char	buf[8*1024];
+    char		*bp = (char *)buf;
 
     printf ("/* runtime-offsets.def\n");
     printf (" *\n");
@@ -47,8 +53,24 @@ int main ()
 
 #include "vproc-offsets-ins.c"
 
+  /* NOTE: the mask value is not used in the basis, but we need to include it to get the
+   * CRC right.
+   */
+    printf ("\n/* mask to get address of VProc from alloc pointer */\n");
+    PR_VALUE(vpMask, VP_MASK, ~((Addr_t)VP_HEAP_SZB-1));
+
+    printf ("\n/* offsets into the log buffer */\n");
+    PR_VALUE(logBufferSzB, LOGBUF_SZB, sizeof(LogBuffer_t));
+    PR_OFFSET(logBuffer, LOGBUF_NEXT_OFFSET, next);
+    PR_OFFSET(logBuffer, LOGBUF_START_OFFSET, log);
+
+    printf ("\n/* offsets into a log event */\n");
+    PR_VALUE(logEventSzB, LOG_EVENT_SZB, sizeof(LogEvent_t));
+    PR_OFFSET(logEvent, LOG_EVENT_KIND_OFFSET, event);
+    PR_OFFSET(logEvent, LOG_EVENT_DATA_OFFSET, data);
+
     printf ("\n/* magic number */\n");
-    printf ("#define MAGIC %#0x\n", CRC32(buf, len));
+    printf ("#define MAGIC %#0x\n", CRC32(buf, bp - (char *)buf));
 
     printf ("#endif /* !_RUNTIME_OFFSETS_DEF_ */\n");
 
