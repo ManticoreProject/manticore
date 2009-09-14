4ea9b1e4116017c9269ab5be7f41351702c04908
jhr <jhr@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2009-09-14 22:41:58 +0000

    Make consistent with trunk
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/branches/swp@4117 ac4fbcad-d21d-0410-b760-c7b1d3380187

--- src/benchmarks/programs/cml-primes/run.sh
+++ src/benchmarks/programs/cml-primes/run.sh
@@ -3,6 +3,13 @@
 # script to run the primes benchmarks
 #
 
+SML=sml
+if [ -x /stage/sml/current/bin/sml ] ; then
+  SML=/stage/sml/current/bin/sml
+fi
+
+PMLFLAGS="-dense"
+
 d=$(date +%Y%m%d-%H%M)
 h=$(hostname)
 
@@ -14,14 +21,14 @@ echo "running CML benchmarks"
 echo "" >> $f
 echo "CML:" >> $f
 for i in 0 1 2 3 4 5 6 7 8 9 ; do
-  /stage/sml/current/bin/sml @SMLload=primes-smlnj >> $f
+  $SML @SMLload=primes-smlnj >> $f
 done
 
 echo "running Manticore 1P benchmarks"
 echo "" >> $f
 echo "Manticore 1P:" >> $f
 for i in 0 1 2 3 4 5 6 7 8 9 ; do
-  primes-mc1 -p 1 >> $f
+  primes-mc1 -p 1 $PMLFLAGS >> $f
 done
 
 #for np in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ; do
@@ -30,8 +37,8 @@ for np in 2 3 4 5 6 7 8 ; do
   echo "running $name benchmarks"
   echo "" >> $f
   echo "$name:" >> $f
-  for i in 0 1 2 3 4 5 6 7 8 9 ; do
-    primes-mc2 -p $np >> $f
+  for i in 0 1 2 3 4 5 6 7 8 9; do
+    primes-mc2 -p $np $PMLFLAGS >> $f
   done
 done
 
@@ -42,8 +49,8 @@ for np in 2 3 4 5 6 7 8 ; do
   echo "running $name benchmarks"
   echo "" >> $f
   echo "$name:" >> $f
-  for i in 0 1 2 3 4 5 6 7 8 9 ; do
-    primes-mc3 -p $np >> $f
+  for i in 0 1 2 3 4 5 6 7 8 9; do
+    primes-mc3 -p $np $PMLFLAGS >> $f
   done
 done
 
