fb394f9a7d6beb95c9519bfc2fd68c34b35f0605
svenauhagen <svenauhagen@ac4fbcad-d21d-0410-b760-c7b1d3380187>
2010-08-25 16:02:15 +0000

    Proxy Continuation
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/manticore/branches/swp@4614 ac4fbcad-d21d-0410-b760-c7b1d3380187

---
+++ src/lib/parallel-rt/gc/proxy.h
@@ -0,0 +1,13 @@
+
+typedef struct {
+	Value_t	proxyObj;	// proxy object in the global heap
+	Value_t	localObj;	// local-heap object that the proxy represents.
+} ProxyTblEntry_t;
+
+
+extern Value_t createProxy (VProc_t *vp, Value_t fls);
+extern void isProxy (VProc_t *vp,int zahl);
+extern void deleteProxy (VProc_t *vp,int zahl);
+extern int promotedProxy (VProc_t *vp,int zahl);
+extern void setPromote (VProc_t *vp, int zahl);
+extern void createList (VProc_t *vp);
