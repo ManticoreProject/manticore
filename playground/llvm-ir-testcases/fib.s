8a69eb1ec53ea1a6a8fea07f2deb9f4759bb09e6
Waleed Malik <waleed@relcy.com>
2016-06-13 21:30:27 -0500

    adding output from seg-stack option and regular fib

---
+++ playground/llvm-ir-testcases/fib.s
@@ -0,0 +1,67 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 11
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rax
+Ltmp0:
+	.cfi_def_cfa_offset 16
+	movl	$8, %edi
+	callq	_fib
+	movq	%rax, %rcx
+	leaq	L_.str(%rip), %rdi
+	xorl	%eax, %eax
+	movq	%rcx, %rsi
+	callq	_printf
+	xorl	%eax, %eax
+	popq	%rcx
+	retq
+	.cfi_endproc
+
+	.globl	_fib
+	.align	4, 0x90
+_fib:                                   ## @fib
+	.cfi_startproc
+## BB#0:                                ## %entry
+	pushq	%r14
+Ltmp1:
+	.cfi_def_cfa_offset 16
+	pushq	%rbx
+Ltmp2:
+	.cfi_def_cfa_offset 24
+	pushq	%rax
+Ltmp3:
+	.cfi_def_cfa_offset 32
+Ltmp4:
+	.cfi_offset %rbx, -24
+Ltmp5:
+	.cfi_offset %r14, -16
+	movq	%rdi, %rbx
+	cmpq	$2, %rbx
+	jg	LBB1_3
+## BB#1:                                ## %c1
+	movl	$1, %eax
+	jmp	LBB1_2
+LBB1_3:                                 ## %c2
+	leaq	-1(%rbx), %rdi
+	addq	$-2, %rbx
+	callq	_fib
+	movq	%rax, %r14
+	movq	%rbx, %rdi
+	callq	_fib
+	addq	%r14, %rax
+LBB1_2:                                 ## %c1
+	addq	$8, %rsp
+	popq	%rbx
+	popq	%r14
+	retq
+	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"%ld\n"
+
+
+.subsections_via_symbols
