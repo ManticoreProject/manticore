e558e9aa44aa69012930d04a2cd2d330b605aef7
Waleed Malik <waleed@infinite.cs.uchicago.edu>
2016-06-23 12:47:38 -0500

    adding c stackmap struct to read in

--- playground/stackwalker/stackwalker.c
+++ playground/stackwalker/stackwalker.c
@@ -1,4 +1,55 @@
 #include<stdio.h>
+#include<stdint.h>
+
+struct StkSizeRecords {
+	uint64_t functionAddress;
+	uint64_t stackSize;
+};
+
+struct Constants {
+	uint64_t largeConstant;
+};
+
+struct Location {
+	uint8_t registerDirectIndirectConstantConstantIndex;
+	uint8_t reserved;
+	uint16_t dwarfRegNum;
+	int offset;
+};
+
+struct LiveOuts {
+	uint16_t dwarfRegNum;
+	uint8_t reserved;
+	uint8_t size;
+};
+
+struct StkMapRecord {
+	uint64_t patchPointid;
+	uint32_t instructionOffset;
+	uint16_t reserved;
+	uint16_t numLocations;
+	struct Location* locations;
+	uint16_t padding;
+	uint16_t numLiveOuts;
+	struct LiveOuts* liveouts;
+	uint32_t otherPadding;	
+};
+
+struct Header {
+	uint8_t stackMapVersion;
+	uint8_t reservedVal1;
+	uint8_t reservedVal2;
+};
+
+struct StackMap{
+	struct Header header;
+	uint32_t numFunctions;
+	uint32_t numConstants;
+	uint32_t numRecords;
+	struct StkSizeRecords* stackSizeRecords;
+	struct Constants* constants;
+	struct StkMapRecord* stackMapRecord;
+};
 
 extern void stackWalker(int counter){
 	printf("printing from stackwalker. counter is %d\n", counter);
