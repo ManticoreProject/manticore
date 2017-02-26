2c9eb6c682567ea8144f7cfa3ad45a353d90157c
Kavon Farvardin <kavon@farvard.in>
2017-02-25 18:49:06 -0600

    first version of NewStack

--- src/lib/parallel-rt/gc/stacks.c
+++ src/lib/parallel-rt/gc/stacks.c
@@ -14,27 +14,19 @@
 #include "heap.h"
 #include "gc.h"
 #include "internal-heap.h"
+#include "value.h"
 #include <stdio.h>
 
 extern int ASM_DS_Return;
+extern int ASM_DS_ApplyClos;
+extern int ASM_DS_EscapeThrow;
+
+uint64_t invalidRetAddr = 0xDEADACE;
 
 size_t dfltStackSz = 1048576; // TODO make this a parameter of the compiler
 
-Value_t NewStack (VProc_t *vp, Value_t finishClos, Value_t funClos) {
-    StackInfo_t* stkInfo;
-    if (vp->freeStacks == NULL) {
-        // get a fresh stack
-        
-    } else {
-        // pop an existing stack
-        stkInfo = vp->freeStacks;
-        vp->freeStacks = stkInfo->next;
-    }
-    
-    return 0;
-}
-
-StackInfo_t* NewMainStack (VProc_t* vp, void** initialSP) {
+// Retrieves an unused stack for the given vproc.
+StackInfo_t* GetStack(VProc_t *vp) {
     StackInfo_t* info;
     if (vp->freeStacks == NULL) {
         // get a fresh stack
@@ -48,6 +40,39 @@ StackInfo_t* NewMainStack (VProc_t* vp, void** initialSP) {
     // push on alloc'd list
     info->next = vp->allocdStacks;
     vp->allocdStacks = info;
+    
+    return info;
+}
+
+Value_t NewStack (VProc_t *vp, Value_t funClos) {
+    StackInfo_t* info = GetStack(vp);
+    
+    uint64_t* sp = (uint64_t*)(info->initialSP);
+    
+    /* we initialize one frame:
+        high                                            low
+        
+      16-byte                    16-byte
+        v                          v 
+        [invalidRetAddr ][ funClos | &ApplyClos ]
+                        ^                       ^
+                    initial sp             returned stkPtr
+                                                                 
+    */
+    *sp = invalidRetAddr; // funClos should not try to return!
+    *(sp--) = funClos;
+    *(sp--) = &ASM_DS_ApplyClos;
+    
+    // now we need to allocate the stack cont object
+    Value_t resumeK = AllocStkCont(vp, (Addr_t)&ASM_DS_EscapeThrow,
+                                        sp, // stack ptr
+                                        info); // stack info
+    
+    return resumeK;
+}
+
+StackInfo_t* NewMainStack (VProc_t* vp, void** initialSP) {
+    StackInfo_t* info = GetStack(vp);
     
     // initialize stack for a return from manticore's main fun.
     void* stkPtr = info->initialSP;
