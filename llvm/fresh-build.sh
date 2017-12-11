9d6141fd71afc983b131b4532456622ec02bc28e
Kavon Farvardin <kavon@farvard.in>
2017-12-11 16:20:24 -0600

    handy script for building llvm

---
+++ llvm/fresh-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# assumes that current directory is the root of the repository.
+
+pushd llvm
+rm -rf build install
+mkdir build install
+pushd build
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" ../src
+ninja opt llc
