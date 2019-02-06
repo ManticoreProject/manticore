83427dbc0493af85a12a4fb1e3c080ff4ecb331e
Kavon Farvardin <kavon@farvard.in>
2019-02-06 15:49:35 -0600

    *.bsh -> *.sh for scripts

--- run_ci.sh
+++ run_ci.sh
@@ -62,7 +62,7 @@ rm -f $CI_REPORT
 touch $CI_REPORT
 
 # $1 = config
-# $2 = script, e.g., run-seq.bsh
+# $2 = script, e.g., run-seq.sh
 # $3 = aout flags
 runTest () {
     AOUTFLAGS="$3" BACKEND="$1" $TIMEOUT 20m ./src/regression-tests/bash-scripts/$2
@@ -77,15 +77,15 @@ for stack in "${stacks[@]}"; do
     for llvm in  "${llvmOptions[@]}"; do
         config="$llvm $stack"
         echo -e "\n\n\t----- testing configuration: $config -----\n\n"
-        runTest "$config" run-seq.bsh
-        runTest "$config" run-cml.bsh "-p 1"
+        runTest "$config" run-seq.sh
+        runTest "$config" run-cml.sh "-p 1"
         echo -e "\n\n\t----- done -----\n\n"
     done
 done
 
 echo -e "\n\n\t----- testing with MLRISC -----\n\n"
-runTest -mlrisc run-seq.bsh
-runTest -mlrisc run-cml.bsh
+runTest -mlrisc run-seq.sh
+runTest -mlrisc run-cml.sh
 echo -e "\n\n\t----- done -----\n\n"
 
 # Exit with error if any tests failed
